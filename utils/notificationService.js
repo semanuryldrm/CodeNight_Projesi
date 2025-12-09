167605a6f729a32cfa91c6ab809cbdb16fb91c34
ilayda öztürk <153443090+ilydozttrk@users.noreply.github.com>
2025-12-09 04:20:42 +0300

    Fix string interpolation in notification messages

--- utils/notificationService.js
+++ utils/notificationService.js
@@ -1,9 +1,11 @@
 const logger = require('./logger');
 
 exports.notifyTicketResolved = async (ticket, user) => {
-    logger.info([NOTIFICATION] Email sent to ${user.email}: Ticket #${ticket.id} çözüldü.);
+    // DÜZELTME: Metin backtick (`) içine alındı
+    logger.info(`[NOTIFICATION] Email sent to ${user.email}; Ticket #${ticket.id} çözüldü.`);
 };
 
 exports.notifyCommentAdded = async (ticket, user, comment) => {
-    logger.info([NOTIFICATION] Email sent to ${user.email}: Ticket #${ticket.id} yeni yorum var.);
+    // DÜZELTME: Metin backtick (`) içine alındı
+    logger.info(`[NOTIFICATION] Email sent to ${user.email}; Ticket #${ticket.id} yeni yorum var.`);
 };
